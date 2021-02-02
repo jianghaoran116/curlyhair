40bd44533943d95d3eda6cbfa5d615baee9b67b4
jianghaoran116 <jianghaoran@outlook.com>
2021-02-02 09:35:49 +0800

    设置版本号

--- biz-cli/cli-dev/core/cli/lib/index.js
+++ biz-cli/cli-dev/core/cli/lib/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = core;
+const path = require('path');
 const userHome = require('user-home');
 const colors = require('colors/safe');
 const semver = require('semver');
@@ -10,12 +10,18 @@ const log = require('@curlyhair-biz-cli-dev/log')
 const pkg = require("../package.json");
 const constant = require('./const');
 
-function core() {
+let args;
+
+async function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
     checkRoot();
     checkUserHome();
+    checkInputArgs();
+    checkEnv();
+    await checkGlobalUpdate();
+    log.verbose('debug', 'test debug log');
   } catch (e) {
     log.error(e.message);
   }
@@ -50,3 +56,58 @@ function checkRoot() {
 function checkPkgVersion() {
   log.info("cli-version", pkg.version);
 }
+
+// 检查入参 - 看看是不是需要进入调试模式
+function checkInputArgs() {
+  const minimist = require('minimist');
+  args = minimist(process.argv.slice(2));
+  checkArgs();
+}
+
+function checkArgs() {
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  } else {
+    process.env.LOG_LEVEL = 'info';
+  }
+  log.level = process.env.LOG_LEVEL;
+}
+
+function checkEnv() {
+  const dotenv = require('dotenv');
+  const dotenvPath = path.resolve(userHome, '.env');
+  if (pathExists(dotenvPath)) {
+    dotenv.config({
+      path: dotenvPath,
+    });
+  }
+  createDefaultConfig();
+  log.verbose('环境变量', process.env.CLI_HOME_PATH);
+}
+
+// 设置默认的环境变量
+function createDefaultConfig() {
+  const cliConfig = {
+    home: userHome,
+  };
+  if (process.env.CLI_HOME) {
+    cliConfig['cliHome'] = path.join(userHome, process.env.CLI_HOME);
+  } else {
+    cliConfig['cliHome'] = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+  process.env.CLI_HOME_PATH = cliConfig.cliHome;
+}
+
+async function checkGlobalUpdate() {
+  const currentVersion = pkg.version;
+  const npmName = pkg.name;
+  const { getNpmSemverVersion } = require('@curlyhair-biz-cli-dev/get-npm-info');
+  const lastVersion = await getNpmSemverVersion(currentVersion, npmName);
+  console.log('lastVersion:::', lastVersion)
+  if (lastVersion && semver.gt(lastVersion, currentVersion)) {
+    log.warn(colors.yellow(`请手动更新 ${npmName}，当前版本：${currentVersion}，最新版本：${lastVersion}
+                更新命令： npm install -g ${npmName}`));
+  }
+}
+
+module.exports = core;
